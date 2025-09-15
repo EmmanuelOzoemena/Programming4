a27240a10695f713595878e92dab7eeecaf864e0
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-09-15 23:19:20 +0100

    Practice some js codes

---
+++ js-basics-mosh/index.js
@@ -0,0 +1,43 @@
+// This is  my first JavaScript code for the nth time
+console.log("Hello World!");
+
+// Variable
+let name = "Ebuka";
+console.log(name);
+
+let firstName = "Ebuka";
+let lastName = "Ozoemena";
+
+// Constant - const
+const interestRate = 0.3;
+// interestRate = 1;
+console.log(interestRate);
+
+// Primitive Type
+let middleName = "Emmanuel"; // String Literal
+let age = 20; // Number Literal
+let isApproved = true; // Boolean Literal
+let notApproved = false; // Boolean Literal
+let surname = undefined;
+let nothing = null;
+
+// Objects
+let person = {
+  name: "Ebuka",
+  age: 20,
+};
+// Dot Notation
+person.name = "Emmanuel";
+
+// Bracket Notation
+let selection = "name";
+person[selection] = "Mary";
+
+console.log(person.name);
+
+// Arrays
+let selectedColors = ["red", "blue"]; // Array Literal
+selectedColors[2] = 1;
+console.log(selectedColors.length);
+
+// Functions
