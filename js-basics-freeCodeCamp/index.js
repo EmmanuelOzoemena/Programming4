fc389002085e531b654f6299c2543d84f7f1dca6
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-09-17 01:10:09 +0100

    Practiced Arrays

--- js-basics-freeCodeCamp/index.js
+++ js-basics-freeCodeCamp/index.js
@@ -34,7 +34,6 @@ let lastLetterofFirstName = firstName[firstName.length - 1];
 console.log(lastLetterofFirstName);
 
 // Word Blank
-
 function worldBlanks(myNoun, myAdjective, myVerb, myAdverb) {
   let result = "";
 
@@ -47,6 +46,64 @@ console.log(worldBlanks("dog", "big", "ran", "quickly"));
 console.log(worldBlanks("bike", "slow", "flew", "slowly"));
 
 // Array
-
 let ourArray = ["Ebuka", 20];
 console.log(ourArray);
+
+let nestedArray = [
+  ["Ebuka", 20],
+  ["John", 45],
+];
+
+console.log(nestedArray);
+
+let ourData = [50, 60, 70];
+console.log(ourData[0]);
+
+ourData[1] = 45; // Modifying arrays with index
+console.log(ourData);
+
+// Access Multi-Dimensional arrays with indexes
+let multiArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+console.log(multiArray);
+console.log(multiArray[2][1]);
+
+// Manipulate array with .push() - adds to an array (from behind)
+let randomArray = ["Stimpson", "J", "Cat"];
+randomArray.push(["happy", "joy"]);
+
+console.log(randomArray);
+
+// Manipulate array with .pop() - removes the last element of an array
+let numArray = [1, 2, 3];
+let removeLastArray = numArray.pop();
+
+console.log(removeLastArray);
+console.log(numArray);
+
+// Manipulate array with .shift() - removes the first element of an array
+let removeFirstArray = numArray.shift();
+console.log(removeFirstArray);
+console.log(numArray);
+
+// Manipulate array with .unshift() - adds to an array (from the beginning of an array)
+let someArray = ["Ebuka", "Hello", 20];
+someArray.shift();
+someArray.unshift("Happy");
+
+console.log(someArray);
+
+// Example - Shopping List
+let myList = [
+  ["Cereals", 3],
+  ["Milk", 2],
+  ["Banana", 3],
+  ["Juice", 2],
+  ["Eggs", 12],
+];
+
+console.log(myList)
